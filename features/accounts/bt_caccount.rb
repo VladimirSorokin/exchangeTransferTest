339803a0a847f9e8a9571e96ca48c3e4240f4e48
Vladimir Sorokin <v.sorokin@i-free.local>
2017-11-01 17:36:48 +0300

    fix

--- features/accounts/bt_caccount.rb
+++ features/accounts/bt_caccount.rb
@@ -19,13 +19,13 @@ class BTCAccount
 
   def get_balance
     res = Typhoeus::Request.get(@config['URL'], :headers=>{'Content-type'=>'application/json', 'X-Api-Key'=>@config['API_KEY']})
-    # for value in Oj.load(res.body, Oj.default_options)
-    #   puts 'Check value!'
-    #   if value['currency'] == 'BTC'
-    #     puts value['balance']
-    #     return value['balance']
-    #   end
-    # end
+    for value in Oj.load(res.body, Oj.default_options)
+      puts 'Check value!'
+      if value['currency'] == 'BTC'
+        puts value['balance']
+        return value['balance']
+      end
+    end
   end
 
   def get_uuid
--- features/accounts/usd_account.rb
+++ features/accounts/usd_account.rb
@@ -19,12 +19,12 @@ class USD_account
 
   def get_balance
     res = Typhoeus::Request.get(@config['URL'], :headers=>{'Content-type'=>'application/json', 'X-Api-Key'=>@config['API_KEY']})
-    # for value in Oj.load(res.body, Oj.default_options)
-    #   puts 'Check value!'
-    #   if value['currency'] == 'USD'
-    #     return value['balance']
-    #   end
-    # end
+    for value in Oj.load(res.body, Oj.default_options)
+      puts 'Check value!'
+      if value['currency'] == 'USD'
+        return value['balance']
+      end
+    end
   end
 
   def get_uuid
