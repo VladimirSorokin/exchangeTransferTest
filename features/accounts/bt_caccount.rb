f3ca331ca21e51b73798aa8d56f4830551743ea9
Vladimir Sorokin <v.sorokin@i-free.local>
2017-11-01 17:40:59 +0300

    fix

--- features/accounts/bt_caccount.rb
+++ features/accounts/bt_caccount.rb
@@ -18,14 +18,14 @@ class BTCAccount
   end
 
   def get_balance
-    res = Typhoeus::Request.get(@config['URL'], :headers=>{'Content-type'=>'application/json', 'X-Api-Key'=>@config['API_KEY']})
-    for value in Oj.load(res.body, Oj.default_options)
-      puts 'Check value!'
-      if value['currency'] == 'BTC'
-        puts value['balance']
-        return value['balance']
-      end
-    end
+    res1 = Typhoeus::Request.get(@config['URL'], :headers=>{'Content-type'=>'application/json', 'X-Api-Key'=>@config['API_KEY']})
+    return res1.body
+    # for value in Oj.load(res.body, Oj.default_options)
+    #   if value['currency'] == 'BTC'
+    #     puts value['balance']
+    #     return value['balance']
+    #   end
+    # end
   end
 
   def get_uuid
